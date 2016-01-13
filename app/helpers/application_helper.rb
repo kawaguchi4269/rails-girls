acf7baf5672c683a1b00a8420cc98bebd6d90353
kawaguchi.takahiro <kawaguchi.takahiro@vareal.co.jp>
2016-01-13 19:13:12 +0000

    ナビゲーションへpagesを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  #現在のページがactiveかどうか判別
+  # 参考元 http://qiita.com/kawahiro311/items/5547d562a29127bdf452
+  def active_class_for controller_name
+    'active' if params[:controller] == controller_name
+  end
 end
